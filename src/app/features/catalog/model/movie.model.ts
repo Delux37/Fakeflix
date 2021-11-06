1b983d46b78edea15c4442b29872ae1c86a3f151
Deluxe <giobibiluri.gb@gmail.com>
2021-11-06 22:08:39 +0400

    feat(search) - add movie-cards

--- src/app/features/catalog/model/movie.model.ts
+++ src/app/features/catalog/model/movie.model.ts
@@ -7,6 +7,7 @@ export interface Movie {
   origin_country: string[];
   original_language: string;
   original_name: string;
+  original_title: string;
   overview: string;
   popularity: number;
   poster_path: string;
--- src/app/features/search/search.component.ts
+++ src/app/features/search/search.component.ts
@@ -1,28 +1,37 @@
+import { CatalogService } from './../catalog.service';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { debounceTime, map, switchMap, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Movie } from '../catalog/model/movie.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
+  providers: [CatalogService],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SearchComponent implements OnInit {
-
-  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+  image = environment.image;
+  
+  constructor(private route: ActivatedRoute, private http: HttpClient, public catalogService: CatalogService) { }
+  searchKeyword$ = new BehaviorSubject<string>('');
+  searchMovies$ = new BehaviorSubject<Movie[] | null>(null);
 
   ngOnInit(): void {
-    this.route.queryParams
+  this.route.queryParams
     .pipe(
       debounceTime(500),
-      switchMap(e => this.fetch('search/multi', `&language=en-US&query=${e.searchTerm}`)
-      )
-    )
-    .subscribe(films => console.log(films))
+      tap(keyword => this.searchKeyword$.next(keyword.searchTerm)),
+      switchMap(e => this.fetch('search/multi', `&language=en-US&query=${e.searchTerm}`)), 
+      tap(movies => {
+        this.searchMovies$.next(movies);
+      })
+    ).subscribe(e => e.forEach(el => console.log(el.original_name)));
   }
   fetch(type: string, filter?: string): Observable<Movie[]> {
     return this.http
